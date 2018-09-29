bab67db3ed728ff9a560cc116541dbf67529d0ed
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-29 06:30:57 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,27 +1,17 @@
 class Patient
-  @@all = []
-  attr_accessor :name
-
-  def initialize(name)
-    @@all << self
-    @name = name
-    @appointments = []
-  end
-
- def self.all
-    @@all
-  end 
-
-  def new_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self
-  end
-
-  def appointments
-    @appointments
-  end
-
-  def doctors
-    @appointments.collect { |appointment| appointment.doctor }
-  end
+	attr_accessor :name
+ 	def initialize(name)
+		@name = name
+		@appointments = []
+	end
+ 	def add_appointment(appointment)
+		@appointments << appointment
+		appointment.patient = self
+	end
+ 	def appointments
+		@appointments
+	end
+ 	def doctors
+		self.appointments.collect { |appointment| appointment.doctor }
+	end
 end
