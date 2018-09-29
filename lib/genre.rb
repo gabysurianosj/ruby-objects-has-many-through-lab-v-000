c76aa8fcfd9e9e0bf3d9c1e68d806f740afc4838
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-29 06:30:46 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,26 +1,18 @@
 class Genre
-  @@all = []
-  attr_accessor :name
-
-  def initialize(name)
-    @@all << self 
-    @name = name
-    @songs = []
-  end
-
-  def self.all
-   @@all
-  end 
- 
-  def new_song(song)
-    @songs << song
-  end
-
-  def songs
-    @songs
-  end
-
-  def artists
-    @songs.collect { |song| song.artist }
-  end
+	attr_accessor :name
+ 	def initialize(name)
+		@name = name
+		@songs = []
+	end
+ 	def add_song(song)
+		@songs << song
+	end
+ 	def songs
+		@songs
+	end
+ 	def artists
+		self.songs.collect do |song|
+			song.artist
+		end
+	end
 end
