e5b5d3cee21736a48890d0b13d95b2eff27f3d7b
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-29 06:36:24 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment
 	attr_accessor :date, :doctor, :patient
- 	def initialize(doctor, date)
+ 	def initialize(patient, doctor, date)
 		@date = date
 		@doctor = doctor
 		doctor.add_appointment(self)
